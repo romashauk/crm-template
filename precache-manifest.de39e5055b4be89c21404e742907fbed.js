3722a8f554a1970f540357a39d6e78176d705ed6
roma.shauk <roman.shauk@dataprobsoton.com>
2019-08-03 20:46:21 +0300

    Updates

---
+++ precache-manifest.de39e5055b4be89c21404e742907fbed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d25e8f2a9081555644274f1dc98e14",
+    "url": "/crm-template/index.html"
+  },
+  {
+    "revision": "2704c3400b147cc8823c",
+    "url": "/crm-template/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "62149d58f59a0e507a00",
+    "url": "/crm-template/static/css/main.003d54ff.chunk.css"
+  },
+  {
+    "revision": "2704c3400b147cc8823c",
+    "url": "/crm-template/static/js/2.1d9f59e1.chunk.js"
+  },
+  {
+    "revision": "62149d58f59a0e507a00",
+    "url": "/crm-template/static/js/main.0da45b1d.chunk.js"
+  },
+  {
+    "revision": "3eb0cc21ca67225eddd1",
+    "url": "/crm-template/static/js/runtime~main.e0db54cc.js"
+  }
+]);
